86ef6f4393bf888d6a04569d4bbfeccf96a089a0
Filip Kozlik <filmatkozlik@gmail.com>
2025-07-03 10:47:24 +0200

    Vite changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/porfolio-pages/',
   server: {
-    base: '/porfolio-pages/',
     host: "::",
     port: 8080,
   },
