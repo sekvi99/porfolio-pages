b182a90619046f50fc999e0df7131a29aafcfa01
Filip Kozlik <filmatkozlik@gmail.com>
2025-07-03 11:20:00 +0200

    Adjusted config for new name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/porfolio-pages/" : "/",
+  base: mode === "production" ? "/website/" : "/",
   server: {
     host: "::",
     port: 8080,
